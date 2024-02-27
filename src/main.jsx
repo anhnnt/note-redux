610e832834adf0e490a2f7277c4a32b174ba3813
Tram Anh <anhnguyen@anhon.local>
2024-02-27 15:29:30 +0200

    combined reducer and set filter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import ReactDOM from 'react-dom/client'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
+import { combineReducers } from 'redux'
 
 import App from './App'
-import noteReducer from './reducers/noteReducer'
+import noteReducer, { createNote } from './reducers/noteReducer'
+import filterReducer, { filterChange } from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
 
 const store = configureStore({
-  reducer: noteReducer
+  reducer: reducer
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
